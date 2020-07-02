ab28e7622710d267bda6991aa854086ae4d3665f
dilmihir123 <dilmihirdil123@gmail.com>
2020-07-02 19:00:40 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Maincomponents from './Components/Maincomponents';
+import './App.css';
+
+function App() {
+  return (
+    <Maincomponents/>
+  );
+}
+
+export default App;
